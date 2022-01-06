2020f65d2d66b3c8af3adf8e688c73e532e9be4b
shivamsalu17 <shivsalu17@gmail.com>
2022-01-06 15:15:06 +0530

    js and header chnages

---
+++ header.php
@@ -0,0 +1,74 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <h1 class="logo"><a href="index.html"><b>MIS</b></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+
+                <li class="dropdown"><a href="#"><span>Energy</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="energy.php">Electricity</a></li>
+                    </ul>
+                </li>
+
+                <li><a class="nav-link scrollto">Transport</a></li>
+
+                <li class="dropdown"><a href="#"><span>AFOLU</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Agriculture</a></li>
+                        <li><a href="#">Forest</a></li>
+                        <li><a href="#">Landuse</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-link scrollto">SolidWaste</a></li>
+                <li><a class="nav-link scrollto">WasteWater</a></li>
+                <li class="dropdown"><a href="#"><span>Industry</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Energy</a></li>
+                        <li><a href="#">Process & Product</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown"><a href="#"><span>FuelType</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Residential</a></li>
+                        <li><a href="#">Commercial</a></li>
+                    </ul>
+                </li>
+                <!-- <li class="dropdown"><a href="#"><span>Other</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </li> -->
+                <!-- <li class="dropdown"><a href="#"><span>AFOLU</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Drop Down 1</a></li>
+                            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                <ul>
+                                    <li><a href="#">Deep Drop Down 1</a></li>
+                                    <li><a href="#">Deep Drop Down 2</a></li>
+                                    <li><a href="#">Deep Drop Down 3</a></li>
+                                    <li><a href="#">Deep Drop Down 4</a></li>
+                                    <li><a href="#">Deep Drop Down 5</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Drop Down 2</a></li>
+                            <li><a href="#">Drop Down 3</a></li>
+                            <li><a href="#">Drop Down 4</a></li>
+                        </ul>
+                    </li> -->
+
+                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                <li><a class="getstarted scrollto" href="login.php">Logout</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
