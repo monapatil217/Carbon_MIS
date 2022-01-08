6336383d8e999ec83bde4a9fab5d222365cc8731
Monika Nemade <nemademonika32@gmail.com>
2022-01-08 10:26:04 +0530

    enegry info page

--- energy.php
+++ energy.php
@@ -56,11 +56,11 @@
                         <div class="card">
                             <div class="card-body">
                                 <h5>
-                                    <p>1 unit of electricity is equal to 1000 watts. which means 1 unit = 1 kwatt
+                                    <!-- <p>1 unit of electricity is equal to 1000 watts. which means 1 unit = 1 kwatt
                                         electricity</p>
                                     <p>Burning 1 kg of bituminous coal will produce 2.42 kg of carbon dioxide</p>
                                     <p>The emissions per unit of electricity are estimated to be in the range of 0.91
-                                        to 0.95 kg/kWh for CO2</p>
+                                        to 0.95 kg/kWh for CO2</p> -->
                                 </h5>
                             </div>
                         </div>
@@ -122,12 +122,95 @@
                     </div>
                 </div>
             </div>
+            
+            <!-- <div class="fade-to-img"  onclick="showEleInfo();">
+                <a href="#" class="electricity" id="electricity"></a><img src="img/idea.png" alt="save-energy" width="80" height="80"> 
+            </div> -->
+
+            <div class="row align-items-center justify-content-center" id="moreInfo">
+
+                <div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+
+                    <div class="popup-flex fade-to-img" onclick="showEleInfo();">
+                        <img class="reggot" id="popup-btn" src="img/idea.png" width="80" height="80">
+                    </div>
+
+                    <div id="popup-wrapper" class="popup-container">
+                        <div class="popup-content">
+
+                            <div class="row align-items-center justify-content-center">
+                                <div id="popUpData" class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <!-- <p>1 unit of electricity is equal to 1000 watts. which means 1 unit = 1 kwatt
+                                        electricity</p>
+                                    <p>Burning 1 kg of bituminous coal will produce 2.42 kg of carbon dioxide</p>
+                                    <p>The emissions per unit of electricity are estimated to be in the range of 0.91
+                                        to 0.95 kg/kWh for CO2</p> -->
+                                </div>
+                                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="btn-container">
+                                        <a href="#" id="close" class="btn-gotit">Got It</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </section><!-- End Hero -->
 
 
     <script>
+
+        var popup = document.getElementById('popup-wrapper');
+        var btn = document.getElementById("popup-btn");
+        var span = document.getElementById("close");
+        btn.onclick = function () {
+            popup.classList.add('show');
+        }
+        span.onclick = function () {
+            popup.classList.remove('show');
+        }
+
+        window.onclick = function (event) {
+            if (event.target == popup) {
+                popup.classList.remove('show');
+            }
+        }
+
+        // var div = document.getElementById("moreInfo");
+        // div.style.display = "none";
+
+    function showEleInfo(){
+        var div = document.getElementById("moreInfo");
+        div.style.display = "block";
+
+        $("#popUpData").empty();
+            var html1 = '<div class="row" >'
+
+                + '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mx-auto "><center><h5 class="mt-4">Electricity </h5></center>'
+
+                + '<div class="row mt-2 mb-3">'
+                + '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'
+                + '<ul style="margin-left: 10px;">'
+                + '<li class="popupli"> Electricity Act 2003 has been enacted and came into force from 15.06.2003.</li>'
+                + '<li class="popupli"> The objective is to introduce competition, protect consumer’s interests and provide power for all.</li>'
+                + '<li class="popupli">Around 60.9% installed generation capacity is due to fossil fuel. </li>'
+                + '<li class="popupli">Around 37.9% installed generation capacity is due to renewable energy sources.</li>'
+                + '<li class="popupli">Around 1.7% installed generation capacity is due to Nuclear Fuel.</li>'
+                + '</ul>'
+                // + '<br>Despite its soaring energy needs, India has one of the lowest per capita rates of consumption of power in the world - 734 units as compared to a world average of 2,429 units. </b></p>'
+                + '<center> <a class="my-3" href="http://www.ghgplatform-india.org/emissionestimates-phase2" target="_blank" rel="noopener noreferrer">Reference</a></center>'
+                + '<center><a class="my-3" href="http://www.technogreen.co.in/Survey/files/Estimates-Energy-National.xlsx" target="_blank" rel="noopener noreferrer">Reference</a></center>'
+
+                + '</div> '
+                + '</div>'
+                + '</div></div>';
+        $("#popUpData").append(html1);
+    }
+
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function() {
         'use strict';
@@ -157,7 +240,14 @@
     <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
     <script src="assets/vendor/php-email-form/validate.js"></script>
 
+     <!-- <script src="assets/js/popper.js"></script> -->
+    <!-- <script src="assets/js/bootstrap.min.js"></script> -->
     <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap-show-modal.js"></script>
+
+    
+
+    
 
 
     <!-- Template Main JS File -->
