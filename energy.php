774b3abc1455f5711c03562894091a58722aa7e8
GRitesh01 <riteshgujar528@gmail.com>
2022-01-08 11:18:54 +0530

    energy unit change

--- energy.php
+++ energy.php
@@ -70,47 +70,58 @@
                 <div class="col-md-12 col-lg-4  mb-3  s" data-aos-delay="200">
                     <div class="in-sec">
                         <h4 class="text-center mb-2">Electricity</h4>
+                        <marquee width="100%" direction="left" height="30px">
+                            ELEC - Electricity / MW - megawatt / m - month.
+                        </marquee>
                         <form class="needs-validation" novalidate>
 
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="eleResi"
-                                    placeholder="Electricity uses in residential area" required>
+                            <div class="input-group mb-3 ">
+                                <span class="form-floating">
+                                    <input type="text" class="form-control" id="eleResi"
+                                        aria-describedby="basic-addon-1" ... />
+                                    <label for="eleResi">Residential ELEC use</label>
+                                </span>
+                                <span class="input-group-text" id="basic-addon-1">MW/m</span>
                                 <div class="invalid-feedback">
                                     Please provide a valid data.
                                 </div>
-                                <label for="eleResi">Electricity uses in residential area</label>
                             </div>
 
-                            <div class="form-floating">
-                                <input type="text" class="form-control" id="eleCom"
-                                    placeholder="Electricity uses in commercial area" required>
+                            <div class="input-group mb-3 ">
+                                <span class="form-floating">
+                                    <input type="text" class="form-control" id="eleCom" aria-describedby="basic-addon-1"
+                                        ... />
+                                    <label for="eleCom">Commercial ELEC use</label>
+                                </span>
+                                <span class="input-group-text" id="basic-addon-1">MW/m</span>
                                 <div class="invalid-feedback">
                                     Please provide a valid data.
                                 </div>
-                                <label for="eleCom">Electricity uses in commercial area</label>
                             </div>
 
-
-
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="eleSum"
-                                    placeholder="Electricity uses in slum area" required>
+                            <div class="input-group mb-3 ">
+                                <span class="form-floating">
+                                    <input type="text" class="form-control" id="eleSum" aria-describedby="basic-addon-1"
+                                        ... />
+                                    <label for="eleSum">Slum area ELEC use</label>
+                                </span>
+                                <span class="input-group-text" id="basic-addon-1">MW/m</span>
                                 <div class="invalid-feedback">
                                     Please provide a valid data.
                                 </div>
-                                <label for="eleSum">Electricity uses in slum area</label>
                             </div>
 
-                            <div class="form-floating">
-                                <input type="text" class="form-control" id="eleSL"
-                                    placeholder="Electricty uses for Street light" required>
+                            <div class="input-group mb-3 ">
+                                <span class="form-floating">
+                                    <input type="text" class="form-control" id="eleSL" aria-describedby="basic-addon-1"
+                                        ... />
+                                    <label for="eleSL">Street light ELEC use</label>
+                                </span>
+                                <span class="input-group-text" id="basic-addon-1">MW/m</span>
                                 <div class="invalid-feedback">
                                     Please provide a valid data.
                                 </div>
-                                <label for="eleSL">Electricty uses for Street light</label>
                             </div>
-
-
 
                             <div class="row ">
                                 <div class="col-md-12 mb-3 text-center">
@@ -122,14 +133,15 @@
                     </div>
                 </div>
             </div>
-            
+
             <!-- <div class="fade-to-img"  onclick="showEleInfo();">
                 <a href="#" class="electricity" id="electricity"></a><img src="img/idea.png" alt="save-energy" width="80" height="80"> 
             </div> -->
 
             <div class="row align-items-center justify-content-center" id="moreInfo">
 
-                <div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                <div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12"
+                    data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
 
                     <div class="popup-flex fade-to-img" onclick="showEleInfo();">
                         <img class="reggot" id="popup-btn" src="img/idea.png" width="80" height="80">
@@ -140,7 +152,7 @@
 
                             <div class="row align-items-center justify-content-center">
                                 <div id="popUpData" class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <!-- <p>1 unit of electricity is equal to 1000 watts. which means 1 unit = 1 kwatt
+                                    <!-- <p>1 unit of electricity is equal to 1000 watts. which means 1 unit = 1 kwatt
                                         electricity</p>
                                     <p>Burning 1 kg of bituminous coal will produce 2.42 kg of carbon dioxide</p>
                                     <p>The emissions per unit of electricity are estimated to be in the range of 0.91
@@ -163,51 +175,54 @@
 
 
     <script>
+    var popup = document.getElementById('popup-wrapper');
+    var btn = document.getElementById("popup-btn");
+    var span = document.getElementById("close");
+    btn.onclick = function() {
+        popup.classList.add('show');
+    }
+    span.onclick = function() {
+        popup.classList.remove('show');
+    }
 
-        var popup = document.getElementById('popup-wrapper');
-        var btn = document.getElementById("popup-btn");
-        var span = document.getElementById("close");
-        btn.onclick = function () {
-            popup.classList.add('show');
-        }
-        span.onclick = function () {
+    window.onclick = function(event) {
+        if (event.target == popup) {
             popup.classList.remove('show');
         }
+    }
 
-        window.onclick = function (event) {
-            if (event.target == popup) {
-                popup.classList.remove('show');
-            }
-        }
+    // var div = document.getElementById("moreInfo");
+    // div.style.display = "none";
 
-        // var div = document.getElementById("moreInfo");
-        // div.style.display = "none";
-
-    function showEleInfo(){
+    function showEleInfo() {
         var div = document.getElementById("moreInfo");
         div.style.display = "block";
 
         $("#popUpData").empty();
-            var html1 = '<div class="row" >'
+        var html1 = '<div class="row" >'
 
-                + '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mx-auto "><center><h5 class="mt-4">Electricity </h5></center>'
+            +
+            '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mx-auto "><center><h5 class="mt-4">Electricity </h5></center>'
 
-                + '<div class="row mt-2 mb-3">'
-                + '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'
-                + '<ul style="margin-left: 10px;">'
-                + '<li class="popupli"> Electricity Act 2003 has been enacted and came into force from 15.06.2003.</li>'
-                + '<li class="popupli"> The objective is to introduce competition, protect consumer’s interests and provide power for all.</li>'
-                + '<li class="popupli">Around 60.9% installed generation capacity is due to fossil fuel. </li>'
-                + '<li class="popupli">Around 37.9% installed generation capacity is due to renewable energy sources.</li>'
-                + '<li class="popupli">Around 1.7% installed generation capacity is due to Nuclear Fuel.</li>'
-                + '</ul>'
-                // + '<br>Despite its soaring energy needs, India has one of the lowest per capita rates of consumption of power in the world - 734 units as compared to a world average of 2,429 units. </b></p>'
-                + '<center> <a class="my-3" href="http://www.ghgplatform-india.org/emissionestimates-phase2" target="_blank" rel="noopener noreferrer">Reference</a></center>'
-                + '<center><a class="my-3" href="http://www.technogreen.co.in/Survey/files/Estimates-Energy-National.xlsx" target="_blank" rel="noopener noreferrer">Reference</a></center>'
+            +
+            '<div class="row mt-2 mb-3">' +
+            '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">' +
+            '<ul style="margin-left: 10px;">' +
+            '<li class="popupli"> Electricity Act 2003 has been enacted and came into force from 15.06.2003.</li>' +
+            '<li class="popupli"> The objective is to introduce competition, protect consumer’s interests and provide power for all.</li>' +
+            '<li class="popupli">Around 60.9% installed generation capacity is due to fossil fuel. </li>' +
+            '<li class="popupli">Around 37.9% installed generation capacity is due to renewable energy sources.</li>' +
+            '<li class="popupli">Around 1.7% installed generation capacity is due to Nuclear Fuel.</li>' +
+            '</ul>'
+            // + '<br>Despite its soaring energy needs, India has one of the lowest per capita rates of consumption of power in the world - 734 units as compared to a world average of 2,429 units. </b></p>'
+            +
+            '<center> <a class="my-3" href="http://www.ghgplatform-india.org/emissionestimates-phase2" target="_blank" rel="noopener noreferrer">Reference</a></center>' +
+            '<center><a class="my-3" href="http://www.technogreen.co.in/Survey/files/Estimates-Energy-National.xlsx" target="_blank" rel="noopener noreferrer">Reference</a></center>'
 
-                + '</div> '
-                + '</div>'
-                + '</div></div>';
+            +
+            '</div> ' +
+            '</div>' +
+            '</div></div>';
         $("#popUpData").append(html1);
     }
 
@@ -240,14 +255,14 @@
     <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
     <script src="assets/vendor/php-email-form/validate.js"></script>
 
-     <!-- <script src="assets/js/popper.js"></script> -->
+    <!-- <script src="assets/js/popper.js"></script> -->
     <!-- <script src="assets/js/bootstrap.min.js"></script> -->
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap-show-modal.js"></script>
 
-    
 
-    
+
+
 
 
     <!-- Template Main JS File -->
