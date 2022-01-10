0e10211121b609b22fcaf68387c6af5f42d681a9
aminbhaidev <49154006+aminbhaidev@users.noreply.github.com>
2022-01-10 16:39:02 +0530

    data

--- js/login.js
+++ js/login.js
@@ -1,13 +1,37 @@
 $(document).ready(function () {
-    getCity();
+    
 })
-
+function user(){
+    $("#loginfrom").empty();
+    var html = '<div class="row mx-auto mt-5  justify-content-center">'
+               +'<div class="col-md-6 mb-3">'
+               +'<label for="username" class="form-label">City</label>'
+               +'<select class="form-select" id="username" name="username" required>'
+               + '<option selected disabled value="">Choose City</option>'
+               + '</select>'
+               + '<div class="invalid-feedback">Please select a City.</div>'
+               + '</div>'
+               + '</div>'
+               $("#loginfrom").append(html);
+               getCity();
+}
+function admin(){
+    $("#loginfrom").empty();
+    var html = '<div class="row mt-5 justify-content-center">'
+               +'<div class="col-md-6 mb-3">'
+               +'<label for="username">User Id</label>'
+               +'<input type="text" class="form-control"  id="username" name="username" required>'
+               + '<div class="invalid-feedback">Please provide user Id.</div>'
+               + '</div>'
+               + '</div>'
+               $("#loginfrom").append(html);
+              // getCity();
+}
 function getCity() {
     $("#username").empty();
     var cityName = getCityList();
-
     var html = '<option selected disabled value="">Choose City</option>'
-        + cityName;
+              + cityName;
 
     $("#username").append(html);
 
