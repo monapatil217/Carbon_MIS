37f8d8f324fbdddaa98f46b73fb510049a88ccc2
shivamsalu17 <shivsalu17@gmail.com>
2022-01-10 12:33:02 +0530

    landuse

--- landUSe.php
+++ landUSe.php
@@ -17,9 +17,7 @@
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
@@ -39,6 +37,12 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+    <style>
+        #landuseChart {
+            width: 300px;
+            height: 300px;
+        }
+    </style>
 </head>
 
 <body>
@@ -51,16 +55,21 @@
     <section id="hero" class="d-flex  justify-content-center" style="height: auto ; min-height: 100vh;">
         <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
 
-            <div class="row">
+            <input type="text" class="form-control" id="sectionType" value="landuseChart" hidden>
 
+            <div class="row">
                 <div class="col-md-12 col-lg-8  mb-3 s " data-aos-delay="200">
                     <div class="in-sec">
                         <h3>Land Use</h3>
                         <h5>
-                            <p>It has been defined as "the purposes and activities through which people interact 
-                                with land and terrestrial ecosystems"[3] and as "the total of arrangements, activities, 
+                            <p>It has been defined as "the purposes and activities through which people interact
+                                with land and terrestrial ecosystems the total of arrangements, activities,
                                 and inputs that people undertake in a certain land type.</p>
                         </h5>
+                        <div id="chartName">
+                            <h3> Land Use Graph</h3>
+                        </div>
+                        <div id="landuseChart"></div>
                     </div>
                 </div>
 
@@ -76,8 +85,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="residential"
-                                            placeholder="Total area under Residential (sq. km)">
+                                        <input type="text" class="form-control" id="residential" placeholder="Total area under Residential (sq. km)">
                                         <label for="residential">Residential area</label>
                                     </div>
                                 </span>
@@ -91,8 +99,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="commercial"
-                                            placeholder="Total area under Commercial (sq. km)">
+                                        <input type="text" class="form-control" id="commercial" placeholder="Total area under Commercial (sq. km)">
                                         <label for="commercial">Commercial area</label>
                                     </div>
                                 </span>
@@ -106,8 +113,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="waterBodies"
-                                            placeholder="Total area under Commercial (sq. km)">
+                                        <input type="text" class="form-control" id="waterBodies" placeholder="Total area under Commercial (sq. km)">
                                         <label for="waterBodies">Water Bodiea area</label>
                                     </div>
                                 </span>
@@ -122,8 +128,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="defence"
-                                            placeholder="Total area under Defence (sq. km)">
+                                        <input type="text" class="form-control" id="defence" placeholder="Total area under Defence (sq. km)">
                                         <label for="defence">Defence area</label>
                                     </div>
                                 </span>
@@ -137,8 +142,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="agriculture"
-                                            placeholder="Total area under Agriculture (sq. km)">
+                                        <input type="text" class="form-control" id="agriculture" placeholder="Total area under Agriculture (sq. km)">
                                         <label for="agriculture">Agriculture area</label>
                                     </div>
                                 </span>
@@ -152,8 +156,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="vacentLand"
-                                            placeholder="Total area under Vacant Land (sq. km">
+                                        <input type="text" class="form-control" id="vacentLand" placeholder="Total area under Vacant Land (sq. km">
                                         <label for="vacentLand">Vacant Land area</label>
                                     </div>
                                 </span>
@@ -168,8 +171,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="roadArea"
-                                            placeholder="Total area under Road Area (sq. km)">
+                                        <input type="text" class="form-control" id="roadArea" placeholder="Total area under Road Area (sq. km)">
                                         <label for="roadArea">Under Road area</label>
                                     </div>
                                 </span>
@@ -183,8 +185,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="greenArea"
-                                            placeholder="Total area under Green Area (sq. km)">
+                                        <input type="text" class="form-control" id="greenArea" placeholder="Total area under Green Area (sq. km)">
                                         <label for="greenArea">Under Green area</label>
                                     </div>
                                 </span>
@@ -198,8 +199,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="industrial"
-                                            placeholder="Total area under Industrial (sq. km)">
+                                        <input type="text" class="form-control" id="industrial" placeholder="Total area under Industrial (sq. km)">
                                         <label for="industrial">Industrial area</label>
                                     </div>
                                 </span>
@@ -213,8 +213,7 @@
                                 <div class="col-1"></div>
                                 <span class="form-floating">
                                     <div class="form-floating">
-                                        <input type="text" class="form-control" id="slum"
-                                            placeholder="Total area under Slum (sq. km)">
+                                        <input type="text" class="form-control" id="slum" placeholder="Total area under Slum (sq. km)">
                                         <label for="slum">Slum area</label>
                                     </div>
                                 </span>
@@ -236,8 +235,7 @@
                 <!-- Start PopUp div -->
                 <div class="row align-items-center justify-content-center" id="moreInfo">
 
-                    <div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12"
-                        data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                    <div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
 
                         <div class="popup-flex fade-to-img" onclick="showLandUInfo();">
                             <img class="reggot" id="popup-btn" src="img/architect.png" width="80" height="80">
@@ -273,25 +271,33 @@
 
 
     <script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
     </script>
+
+
+
+    <!-- Resources -->
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
 
     <!-- Vendor JS Files -->
     <script src="assets/vendor/purecounter/purecounter.js"></script>
@@ -306,7 +312,7 @@
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap-show-modal.js"></script>
-
+    <script src="js/induGraph.js"></script>
     <!-- Template Main JS File -->
     <script src="assets/js/main.js"></script>
 
