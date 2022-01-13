c0cae699767279c96e6250b5d4c8e1b7a146dbf8
aminbhaidev <49154006+aminbhaidev@users.noreply.github.com>
2022-01-13 11:08:01 +0530

    api add

--- php/getAllData.php
+++ php/getAllData.php
@@ -4,10 +4,11 @@ $json = file_get_contents('php://input');
 $data = json_decode($json);
 $basicid = $data->basicid;
 $type = $data->type;
+$mainArray = array();
     if ($type == "Electricity") {
 
         $query2 = "SELECT * FROM ele_data WHERE b_id='".$basicid."'";
-        $mainArray = array();
+       
         $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
         $rowcount = mysqli_num_rows($result);
         if ($rowcount == 0) {
@@ -15,6 +16,7 @@ $type = $data->type;
             array_push($mainArray, $data);
         } else {
             while ($row = mysqli_fetch_array($result)) {
+                $data = array();
                 $data['data'] = "true";
                 $data['r_elec'] = $row['r_elec'];
                 $data['c_elec'] = $row['c_elec'];
@@ -24,6 +26,7 @@ $type = $data->type;
                 array_push($mainArray, $data);
             }
         }
-        echo  json_encode($mainArray);
+       
     }
+    echo  json_encode($mainArray);
 ?>
