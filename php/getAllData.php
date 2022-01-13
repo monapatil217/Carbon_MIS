a605a0b882995482f9b38d878cd4e44d6209cfc3
aminbhaidev <49154006+aminbhaidev@users.noreply.github.com>
2022-01-13 15:01:55 +0530

    forest and cropland

--- php/getAllData.php
+++ php/getAllData.php
@@ -27,7 +27,49 @@ if ($type == "Electricity") {
             $eleData['s_elec'] = $row['s_elec'];
             $eleData['sl_elec'] = $row['sl_elec'];
             array_push($deleData, $eleData);
-            $mainArray['eleData'] =   $deleData;
+            $mainArray['cData'] =   $deleData;
+            array_push($finalArray, $mainArray);
+        }
+    }
+}else if ($type == "Forest") {
+
+    $query2 = "SELECT * FROM fc_land_data WHERE b_id='" . $basicId . "' AND  type='Forest'";
+
+    $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount == 0) {
+        $mainArray['check'] = "false";
+        array_push($finalArray, $mainArray);
+    } else {
+        while ($row = mysqli_fetch_array($result)) {
+
+            $mainArray['check'] = "true";
+            $deleData = array();
+            $eleData = [];
+            $eleData['area'] = $row['area'];
+            array_push($deleData, $eleData);
+            $mainArray['cData'] =   $deleData;
+            array_push($finalArray, $mainArray);
+        }
+    }
+}else if ($type == "CropLand") {
+
+    $query2 = "SELECT * FROM fc_land_data WHERE b_id='" . $basicId . "' AND  type='CropLand'";
+
+    $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount == 0) {
+        $mainArray['check'] = "false";
+        array_push($finalArray, $mainArray);
+    } else {
+        while ($row = mysqli_fetch_array($result)) {
+
+            $mainArray['check'] = "true";
+            $deleData = array();
+            $eleData = [];
+            $eleData['area'] = $row['area'];
+            array_push($deleData, $eleData);
+            $mainArray['cData'] =   $deleData;
             array_push($finalArray, $mainArray);
         }
     }
