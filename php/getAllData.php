158c76de8cd24e30c654db483deeaff7f6120323
aminbhaidev <49154006+aminbhaidev@users.noreply.github.com>
2022-01-13 15:51:22 +0530

    transport and land use

--- php/getAllData.php
+++ php/getAllData.php
@@ -73,5 +73,108 @@ if ($type == "Electricity") {
             array_push($finalArray, $mainArray);
         }
     }
+}else if ($type == "Transport") {
+
+    $query2 = "SELECT * FROM trans_data WHERE b_id='" . $basicId . "'";
+
+    $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount == 0) {
+        $mainArray['check'] = "false";
+        array_push($finalArray, $mainArray);
+    } else {
+        while ($row = mysqli_fetch_array($result)) {
+
+            $mainArray['check'] = "true";
+            $deleData = array();
+            $eleData = [];
+            $eleData['w2'] = $row['w2'];
+            $eleData['w3'] = $row['w3'];
+            $eleData['w4'] = $row['w4'];
+            $eleData['bus'] = $row['bus'];
+            $eleData['tempo'] = $row['tempo'];
+            $eleData['truck'] = $row['truck'];
+            $eleData['flight'] = $row['flight'];
+            $eleData['train'] = $row['train'];
+            array_push($deleData, $eleData);
+            $mainArray['cData'] =   $deleData;
+            array_push($finalArray, $mainArray);
+        }
+    }
+}else if ($type == "Livestock") { // pending
+
+    $query2 = "SELECT * FROM live_data WHERE b_id='" . $basicId . "'";
+
+    $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount == 0) {
+        $mainArray['check'] = "false";
+        array_push($finalArray, $mainArray);
+    } else {
+        while ($row = mysqli_fetch_array($result)) {
+
+            $mainArray['check'] = "true";
+            $deleData = array();
+            $eleData = [];
+            $eleData['w2'] = $row['w2'];
+            array_push($deleData, $eleData);
+            $mainArray['cData'] =   $deleData;
+            array_push($finalArray, $mainArray);
+        }
+    }
+}else if ($type == "LandUse") { // pending
+
+    $query2 = "SELECT * FROM land_data WHERE b_id='" . $basicId . "'";
+
+    $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount == 0) {
+        $mainArray['check'] = "false";
+        array_push($finalArray, $mainArray);
+    } else {
+        while ($row = mysqli_fetch_array($result)) {
+
+            $mainArray['check'] = "true";
+            $deleData = array();
+            $cData = [];
+            $cData['resi'] = $row['resi'];
+            $cData['vaca'] = $row['vaca'];
+            $cData['com'] = $row['com'];
+            $cData['u_road'] = $row['u_road'];
+            $cData['w_body'] = $row['w_body'];
+            $cData['u_green'] = $row['u_green'];
+            $cData['defence'] = $row['defence'];
+            $cData['indu'] = $row['indu'];
+            $cData['agri'] = $row['agri'];
+            $cData['slum'] = $row['slum'];
+            array_push($deleData, $cData);
+            $mainArray['cData'] =   $deleData;
+            array_push($finalArray, $mainArray);
+        }
+    }
+}else if ($type == "SolidWaste") { // pending
+
+    $query2 = "SELECT * FROM solid_data WHERE b_id='" . $basicId . "'";
+
+    $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount == 0) {
+        $mainArray['check'] = "false";
+        array_push($finalArray, $mainArray);
+    } else {
+        while ($row = mysqli_fetch_array($result)) {
+
+            $mainArray['check'] = "true";
+            $deleData = array();
+            $cData = [];
+            $cData['sw_gen'] = $row['sw_gen'];
+            $cData['sw_col'] = $row['sw_col'];
+            $cData['sw_treat'] = $row['sw_treat'];
+            $cData['n_yard'] = $row['n_yard'];
+            array_push($deleData, $cData);
+            $mainArray['cData'] =   $deleData;
+            array_push($finalArray, $mainArray);
+        }
+    }
 }
 echo  json_encode($finalArray);
