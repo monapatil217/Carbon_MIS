da9b3f298b43b7b72090b64716f22c635fb8caa0
Monika Nemade <nemademonika32@gmail.com>
2022-01-13 12:39:28 +0530

    basic id add in session

--- php/saveBasicInfo.php
+++ php/saveBasicInfo.php
@@ -22,6 +22,8 @@ $id = $_SESSION['cityName'];
 $query = "INSERT INTO basic_info(popu,female,male,city,gArea,tArea,lite,gdp,hospital)
 	VALUES ($popu,$female,$male,'" . $city . "',$gArea,$tArea,$lite,$gdp,$hospital)";
     $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-    
+     $last_id = mysqli_insert_id($conn);
+
+     $_SESSION['basicId'] = $last_id;
     echo  "success";
 ?>
