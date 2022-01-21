b5725a037aad22c71d5579e96c76ccacebfe8eec
aminbhaidev <49154006+aminbhaidev@users.noreply.github.com>
2022-01-21 12:50:40 +0530

    cropland

--- php/savecropland.php
+++ php/savecropland.php
@@ -6,19 +6,22 @@ $data = json_decode($json);
 
 //basic info table
 $basicId = $data->basicId;
-$area = $data->area;
+$perennial = $data->perennial;
+$harwested = $data->harwested;
+$mineralS = $data->mineralS;
+$organicS = $data->organicS;
 
         $query2 = "SELECT * FROM crop_data WHERE b_id='" . $basicId . "'";
         $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
 
         $rowcount = mysqli_num_rows($result);
         if ($rowcount == 0) {
-            $query = "INSERT INTO crop_data(b_id,area)
-            VALUES ($basicId,$area)";
+            $query = "INSERT INTO crop_data(b_id,perennial,harwested,mineralS,organicS)
+            VALUES ($basicId,$perennial,$harwested,$mineralS$organicS)";
             $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
         }else{
-            $query = "UPDATE  crop_data set area=$area
-                       WHERE b_id='".$basicId."'";
+            $query = "UPDATE  crop_data set perennial=$perennial,harwested=$harwested
+                      mineralS=$mineralS,organicS=$organicS WHERE b_id='".$basicId."'";
             $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
         }
         echo  "success";
