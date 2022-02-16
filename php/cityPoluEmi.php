e1aa1aa4fbd1a0c5153bce50124a3a7959bd5f27
GRitesh01 <97169209+GRitesh01@users.noreply.github.com>
2022-02-16 18:32:02 +0530

    api update

--- php/cityPoluEmi.php
+++ php/cityPoluEmi.php
@@ -3,8 +3,16 @@ include 'conn.php';
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 $finalDataArray = [];
-$tableEmiarray = ["ele_emi", "crop_emi", "forest_emi", "fule_emi", "indu_eng_emi", "land_emi", "live_emi", "msw_emi", "trans_emi", "waste_emi"];
-$mtype = ["co2", "ch4", "n2o"];
+$tableEmiarray = array (
+    array("ele_emi"),
+    array("trans_emi"),
+    array("crop_emi","forest_emi","land_emi","live_emi"),
+    array("msw_emi","bmw_emi","hw_emi"),
+    array( "waste_emi"),
+    array("indu_eng_emi","indu_cem_emi","indu_chem_emi","indu_other_emi"),
+    array("fule_emi"),
+   );
+// $mtype = ["co2", "ch4", "n2o"];
 
 $query2 = "SELECT Distinct(name) FROM city_name";
 $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
@@ -15,32 +23,30 @@ while ($row = mysqli_fetch_array($result)) {
     $basicresult = mysqli_query($conn, $basicIdquery)  or die(mysqli_error($conn));
     $rowcount = mysqli_num_rows($basicresult);
     $mainData = array();
+    $tblOfPoluEmi="";
     if ($rowcount == 0) {
-        for ($i = 0; $i < sizeof($mtype); $i++) {
-            $polutype = $mtype[$i];
-            for ($i = 0; $i < sizeof($tableEmiarray); $i++) {
-                $tableName = $tableEmiarray[$i];
-                $mainData['country'] = $cityName;
-
-
-                for ($n = 0; $n < sizeof($mtype); $n++) {
-                    $polutype = $mtype[$n];
-                    $mainData[$polutype] = 0;
-                }
+        for ($i = 0; $i < sizeof($tableEmiarray); $i++) {
+            $tbarray = $tableEmiarray[$i];
+            for($j=0;$j<sizeof($tbarray);$j++){
+                $polutype = $tbarray[$j];
             }
+            $mainData['country'] = $cityName;
+            $mainData[$polutype] = 0;
         }
     } else {
         $row = mysqli_fetch_array($basicresult);
         $basicId = $row['id'];
         $mainData['country'] = $cityName;
 
-        for ($i = 0; $i < sizeof($mtype); $i++) {
-            $polutype = $mtype[$i];
+        for ($i = 0; $i < sizeof($tableEmiarray); $i++) {
+            $polutype = $tableEmiarray[$i];
             $value = 0;
-            for ($j = 0; $j < sizeof($tableEmiarray); $j++) {
-                $tableName = $tableEmiarray[$j];
+            $tbarray=$tableEmiarray[$i];
 
-                $dataQuery = "SELECT ifnull($polutype, 0) as ItemSum FROM $tableName WHERE b_id='" . $basicId . "'";
+            for($j=0;$j<sizeof($tbarray);$j++){
+                $polutype = $tbarray[$j];
+
+                $dataQuery = "SELECT ifnull(ch4, 0) + ifnull(n2o, 0) + ifnull(co2, 0) as ItemSum FROM $polutype WHERE b_id='" . $basicId . "'";
                 $emiresult = mysqli_query($conn, $dataQuery)  or die(mysqli_error($conn));
                 $rowcount = mysqli_num_rows($emiresult);
                 if ($rowcount != 0) {
@@ -48,10 +54,11 @@ while ($row = mysqli_fetch_array($result)) {
                     $value += $row['ItemSum'];
                 }
             }
-
-            $mainData[$polutype] = $value;
+             $mainData['country'] = $cityName;
+             $mainData[$polutype] = round($value,1);
         }
     }
     array_push($finalDataArray, $mainData);
 }
 echo  json_encode($finalDataArray);
+?>
--- php/cityStackEmi.php
+++ php/cityStackEmi.php
@@ -3,7 +3,16 @@ include 'conn.php';
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 $finalDataArray=[];
-$tableEmiarray = ["ele_emi","crop_emi","forest_emi","fule_emi","indu_eng_emi","land_emi","live_emi","msw_emi","trans_emi","waste_emi"];
+$tableEmiarray = array (
+    array("ele_emi"),
+    array("trans_emi"),
+    array("crop_emi","forest_emi","land_emi","live_emi"),
+    array("msw_emi","bmw_emi","hw_emi"),
+    array( "waste_emi"),
+    array("indu_eng_emi","indu_cem_emi","indu_chem_emi","indu_other_emi"),
+    array("fule_emi"),
+   );
+
 $query2 = "SELECT Distinct(name) FROM city_name";
 $result = mysqli_query($conn, $query2)  or die(mysqli_error($conn));
 
@@ -13,27 +22,35 @@ while ($row = mysqli_fetch_array($result)) {
     $basicresult = mysqli_query($conn, $basicIdquery)  or die(mysqli_error($conn));
     $rowcount = mysqli_num_rows($basicresult);
     $mainData = array();
+    $tblOfSec="";
     if ($rowcount == 0) {
         for($i=0;$i<sizeof($tableEmiarray);$i++){
-            $tableName = $tableEmiarray[$i];
+            $tbarray=$tableEmiarray[$i];
+            for($j=0;$j<sizeof($tbarray);$j++){
+                $tblOfSec = $tbarray[$j];
+            }
              $mainData['country'] = $cityName;
-             $mainData[$tableName] = 0;
+             $mainData[$tblOfSec] = 0;
         }
     }else{
         $row = mysqli_fetch_array($basicresult);
         $basicId = $row['id'];
         for($i=0;$i<sizeof($tableEmiarray);$i++){
             $value = 0;
-            $tableName = $tableEmiarray[$i];
-             $dataQuery = "SELECT ifnull(ch4, 0) + ifnull(n2o, 0) + ifnull(co2, 0) as ItemSum FROM $tableName WHERE b_id='".$basicId."'";
-             $emiresult = mysqli_query($conn, $dataQuery)  or die(mysqli_error($conn));
-             $rowcount = mysqli_num_rows($emiresult);
-             if ($rowcount != 0) {
-                $row = mysqli_fetch_array($emiresult);
-                $value = floatval($row['ItemSum']);
-             }
+            $tbarray=$tableEmiarray[$i];
+
+            for($j=0;$j<sizeof($tbarray);$j++){
+                $tblOfSec = $tbarray[$j];
+                $dataQuery = "SELECT ifnull(ch4, 0) + ifnull(n2o, 0) + ifnull(co2, 0) as ItemSum FROM $tblOfSec WHERE b_id='".$basicId."'";
+                $emiresult = mysqli_query($conn, $dataQuery)  or die(mysqli_error($conn));
+                $rowcount = mysqli_num_rows($emiresult);
+                if ($rowcount != 0) {
+                   $row = mysqli_fetch_array($emiresult);
+                   $value += floatval($row['ItemSum']);
+                }
+            }
              $mainData['country'] = $cityName;
-             $mainData[$tableName] = $value;
+             $mainData[$tblOfSec] = round($value,1);
         }
        
     }
